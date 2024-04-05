f79d468942f7f98c2d23fd6531139a022dd75cad
YE-Hordijk <i.hordijk2000@gmail.com>
2024-04-05 05:55:07 -0700

    eerste architectuur

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-import Environment as Env
+import REINFORCE as R
+import ActorCritic as AC
+import gymnasium as gym
+import argparse
 
-observation = Env.env.reset()
-print(observation)
+#import Environment as Env
+#observation = Env.env.reset()
+
+
+#*******************************************************************************
+
+class Learning():
+    def __init__(self, args, max_episodes=300, LearningRate=0.001, gamma=0.99, epsilon=0.9, render_mode="human"):
+        self.max_episodes = max_episodes #100 #300
+        self.Env = gym.make("LunarLander-v2", render_mode="human")
+
+
+#*******************************************************************************
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algorithm", default="REINFORCE", help="Algorithm Options: REINFORCE, ActorCritic")
+    parser.add_argument("--bootstrapping", default="True", help="Bootstrapping Options: True, False")
+    parser.add_argument("--baseline_subtraction", default="True", help="Baseline Subtraction Options: True, False")
+
+    args = vars(parser.parse_args())
+    print(args)
+    print("poep")
+    Learn = Learning(args)
