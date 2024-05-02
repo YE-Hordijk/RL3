880db7df392fff3d57703cf2e4263e868640c244
T.M. Meyer <s2231085@umail.leidenuniv.nl>
2024-05-03 00:43:48 +0200

    Reinforce doet het gwn

--- REINFORCEoud.py
+++ REINFORCEoud.py
@@ -34,55 +34,42 @@ import torch.nn.init as init
 from torch.autograd import Variable
 from torch.distributions import Categorical
 
-
 class Policy_Net(nn.Module):
     def __init__(self):
         super(Policy_Net, self).__init__()
         self.layer1 = nn.Linear(8, 128)
-        self.dropout = nn.Dropout(p=0.6)
-        self.layer2 = nn.Linear(128, 4)
-        #self.layer1 = nn.Linear(8, 128)
-        #self.layer2 = nn.Linear(128, 128)
-        #self.layer3 = nn.Linear(128, 4)
-
-        self.saved_log_probs = []
-        self.rewards = []
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, 4)
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.dropout(x)
-        x = F.relu(x)
-        action_scores = self.layer2(x)
-        return F.softmax(action_scores, dim=1)
-        #x = F.relu(self.layer1(x))
-        #x = F.relu(self.layer2(x))
-        #x = self.layer3(x)
-        #x = F.softmax(x, dim=1)#.to(torch.float64)
-        #return x
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = self.layer3(x)
+        x = F.softmax(x, dim=1)#.to(torch.float64)
+        return x
         
 class REINFORCE():
     def __init__(self):
         self.env = gym.make("LunarLander-v2")#, render_mode="human")
-        self.max_episodes = 1000
+        self.max_episodes = 500
         self.gamma = 0.99
-        self.LearningRate = 0.01
+        self.LearningRate = 0.001
         self.PolicyNet = Policy_Net()
-        self.optimizer = optim.Adam(self.PolicyNet.parameters(), lr=self.LearningRate)
+        self.optimizer = optim.AdamW(self.PolicyNet.parameters(), lr=self.LearningRate, amsgrad=True)
         self.eps = np.finfo(np.float32).eps.item()
-        self.totalrewards = []
+        self.rewards = []
         self.losses = []
         
         
     def select_action(self, state):
         state = torch.from_numpy(state).float().unsqueeze(0)
         probs = self.PolicyNet(state)
-        #print(probs)
-        #entropy = (- probs * torch.log(probs)).sum()
+        print(probs)
+        entropy = (- probs * torch.log(probs)).sum()
         m = Categorical(probs)
         action = m.sample()
-        #log_prob = m.log_prob(action)
-        self.PolicyNet.saved_log_probs.append(m.log_prob(action))
-        return action.item()#, log_prob, entropy
+        log_prob = m.log_prob(action)
+        return action.item(), log_prob, entropy
         
     def select_action_tomke(self, state):
         state = torch.from_numpy(state).unsqueeze(0)
@@ -95,60 +82,38 @@ class REINFORCE():
         log_prob = log_prob[action]
         return action, log_prob, entropy
     
-    def finish_episode(self):
-        R = 0
-        policy_loss = []
-        returns = deque()
-        for r in self.PolicyNet.rewards[::-1]:
-            R = r + self.gamma * R
-            returns.appendleft(R)
-        returns = torch.tensor(returns)
-        returns = (returns - returns.mean()) / (returns.std() + self.eps)
-        for log_prob, R in zip(self.PolicyNet.saved_log_probs, returns):#, entropies):
-            #print(log_prob)
-            policy_loss.append(-log_prob * R)#-(self.LearningRate*entropy))
-        self.optimizer.zero_grad()
-        policy_loss = torch.cat(policy_loss).sum()
-        policy_loss.backward()
-        self.optimizer.step()
-        del self.PolicyNet.rewards[:]
-        del self.PolicyNet.saved_log_probs[:]
-        
     def Reinforce_Learn(self):
-        self.env.reset(seed=543)
-        torch.manual_seed(543)
         for m in range(self.max_episodes):
             state, info = self.env.reset()
             totalreward = 0
-            #state_t = []
-            #action_t = []
-            #reward_t = []
-            #log_probs = []
-            #entropies = []
-            #terminated, truncated = [False, False]
+            state_t = []
+            action_t = []
+            reward_t = []
+            log_probs = []
+            entropies = []
+            terminated, truncated = [False, False]
             print("ep", m)
-            #grad = 0
-            for t in range(1, 1000):#while not (terminated or truncated):
-                action = self.select_action(state)#, log_prob, entropy = self.select_action(state)
+            while not (terminated or truncated):
+                grad = 0
+                action, log_prob, entropy = self.select_action(state)
                 #sample trace h_0{s_0,a_0,r_0,s_1,...,s_n+1} according to policy pi(a|s)
                 #breakpoint()
                 #state_t.append(torch.from_numpy(state).float().unsqueeze(0))
-                state, reward, terminated, truncated, info = self.env.step(action)
+                next_state, reward, terminated, truncated, info = self.env.step(action)
                 #print("rsg", terminated, truncated)
-                #print("tr", totalreward)
-                #reward_t.append(reward)
-                self.PolicyNet.rewards.append(reward)
                 totalreward += reward
-                #log_probs.append(log_prob)
-                #entropies.append(entropy)
-                #action_t.append(action)
+                #print("tr", totalreward)
+                reward_t.append(reward)
+                log_probs.append(log_prob)
+                entropies.append(entropy)
+                action_t.append(action)
                 
-                #state_t.append(torch.from_numpy(state).float().unsqueeze(0))
+                state_t.append(torch.from_numpy(next_state).float().unsqueeze(0))
                 #print(reward_t)
                 
-                #state = next_state
+                state = next_state
                 if (terminated or truncated):
-                    self.totalrewards.append(totalreward)
+                    self.rewards.append(totalreward)
                     break
             '''
             R = torch.zeros(1)
@@ -167,12 +132,28 @@ class REINFORCE():
             #torch.nn.utils.clip_grad_value_(self.PolicyNet.parameters(), 40) # clip gradients to avoid exploding gradients
             self.optimizer.step()
             '''
-            self.finish_episode()
+            R = 0
+            policy_loss = []
+            returns = deque()
+            for r in reward_t[::-1]:
+                R = r + self.gamma * R
+                returns.appendleft(R)
+            returns = torch.tensor(returns)
+            returns = (returns - returns.mean()) / (returns.std() + self.eps)
+            for log_prob, R in zip(log_probs, returns):
+                policy_loss.append(-log_prob * R)
+            self.optimizer.zero_grad()
+            policy_loss = torch.cat(policy_loss).sum()
+            policy_loss.backward()
+            self.optimizer.step()
+
+            #del policy.rewards[:]
+            #del policy.saved_log_probs[:]
             #if(m%25 == 0):
             #    self.env.render()
             #if m % 10 == 0:
                 # play an episode based on your current policy, track the return
-        return self.totalrewards
+        return self.rewards
 
 
 
--- test.py
+++ test.py
@@ -24,10 +24,6 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
 args = parser.parse_args()
 
 
-env = gym.make('LunarLander-v2')
-env.reset(seed=args.seed)
-torch.manual_seed(args.seed)
-
 
 class Policy(nn.Module):
     def __init__(self):
@@ -46,87 +42,93 @@ class Policy(nn.Module):
         action_scores = self.affine2(x)
         return F.softmax(action_scores, dim=1)
 
-
-policy = Policy()
-optimizer = optim.Adam(policy.parameters(), lr=1e-2)
-eps = np.finfo(np.float32).eps.item()
-
-
-def select_action(state):
-    state = torch.from_numpy(state).float().unsqueeze(0)
-    probs = policy(state)
-    m = Categorical(probs)
-    action = m.sample()
-    policy.saved_log_probs.append(m.log_prob(action))
-    print(probs)
-    return action.item()
+class REINFORCE():
+    def __init__(self):
+        self.env = gym.make('LunarLander-v2')
+        self.max_episodes = 500
+        self.gamma = 0.99
+        self.LearningRate = 0.01
+        self.policy = Policy()
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=self.LearningRate)
+        self.eps = np.finfo(np.float32).eps.item()
 
 
-def finish_episode():
-    R = 0
-    policy_loss = []
-    returns = deque()
-    for r in policy.rewards[::-1]:
-        R = r + args.gamma * R
-        returns.appendleft(R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-    for log_prob, R in zip(policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
-    optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_log_probs[:]
+    def select_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.policy(state)
+        m = Categorical(probs)
+        action = m.sample()
+        self.policy.saved_log_probs.append(m.log_prob(action))
+        #print(probs)
+        return action.item()
 
 
-def main():
-    running_reward = 10
-    totalrewards = []
-    for i_episode in range(500):#count(1):
-        state, _ = env.reset()
-        ep_reward = 0
-        for t in range(1, 1000):  # Don't infinite loop while learning
-            action = select_action(state)
-            state, reward, done, _, _ = env.step(action)
-            if args.render:
-                env.render()
-            policy.rewards.append(reward)
-            ep_reward += reward
-            if done:
-                totalrewards.append(ep_reward)
+    def finish_episode(self):
+        R = 0
+        policy_loss = []
+        returns = deque()
+        for r in self.policy.rewards[::-1]:
+            R = r + self.gamma * R
+            returns.appendleft(R)
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + self.eps)
+        for log_prob, R in zip(self.policy.saved_log_probs, returns):
+            #print(log_prob)
+            policy_loss.append(-log_prob * R)
+        self.optimizer.zero_grad()
+        policy_loss = torch.cat(policy_loss).sum()
+        policy_loss.backward()
+        self.optimizer.step()
+        del self.policy.rewards[:]
+        del self.policy.saved_log_probs[:]
+
+
+    def main(self):
+        running_reward = 10
+        totalrewards = []
+        for i_episode in range(self.max_episodes):#count(1):
+            state, _ = self.env.reset()
+            ep_reward = 0
+            for t in range(1, 1000):  # Don't infinite loop while learning
+                action = self.select_action(state)
+                state, reward, terminated, truncated, _ = self.env.step(action)
+                self.policy.rewards.append(reward)
+                ep_reward += reward
+                if (terminated or truncated):
+                    totalrewards.append(ep_reward)
+                    break
+
+            running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+            self.finish_episode()
+            if i_episode % args.log_interval == 0:
+                print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+                      i_episode, ep_reward, running_reward))
+            if running_reward > self.env.spec.reward_threshold:
+                print("Solved! Running reward is now {} and "
+                      "the last episode runs to {} time steps!".format(running_reward, t))
                 break
-
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        finish_episode()
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                  i_episode, ep_reward, running_reward))
-        if running_reward > env.spec.reward_threshold:
-            print("Solved! Running reward is now {} and "
-                  "the last episode runs to {} time steps!".format(running_reward, t))
-            break
-    
         
-    plt.plot(range(len(totalrewards)), totalrewards)
-    plt.xlabel("Episode")
-    plt.ylabel("Timestep")
-    plt.show()
-    plt.savefig("test.png")
             
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+        plt.plot(range(len(totalrewards)), totalrewards)
+        plt.xlabel("Episode")
+        plt.ylabel("Timestep")
+        plt.show()
+        plt.savefig("test.png")
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
         
 
-
-if __name__ == '__main__':
-    main()
+r = REINFORCE() # initialize the models
+policy = r.main()
+print("end", policy)
+#if __name__ == '__main__':
+#    main()
